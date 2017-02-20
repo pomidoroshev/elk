1ae538f65bf2f89a20c1048ce6e65d678d45c564
Dmitry Doroshev <r.schweppes@ya.ru>
2017-02-20 14:35:01 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='elk',
+    version='0.0.1',
+    description='ELK Logging Supporg',
+    url='http://github.com/pomidoroshev/elk',
+    author='Dmitry Doroshev',
+    author_email='dima@doroshev.com',
+    license='MIT',
+    zip_safe=False,
+)
