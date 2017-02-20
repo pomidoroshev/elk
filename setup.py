6364cb7f66f7c8b696a321d80e8e60f384f11ba1
Dmitry Doroshev <r.schweppes@ya.ru>
2017-02-20 14:40:58 +0300

    Add `packages` param

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='elk',
     version='0.0.1',
+    packages=['elk'],
     description='ELK Logging Supporg',
     url='http://github.com/pomidoroshev/elk',
     author='Dmitry Doroshev',
