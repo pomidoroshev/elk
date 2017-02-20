254606b09a8e62845a09ea9c22173584b5ddc896
Dmitry Doroshev <r.schweppes@ya.ru>
2017-02-20 15:16:17 +0300

    Add default settings

---
+++ elk/settings.py
@@ -0,0 +1,31 @@
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'debug': {
+            'level': 'DEBUG',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+        },
+        'elasticsearch': {
+            'level': 'INFO',
+            'formatter': 'standard',
+            'class': 'elk.BulkUdp',
+            'host': os.getenv('ELK_SERVER'),
+            'port': int(os.getenv('ELK_PORT')),
+            'service': os.getenv('ELK_SERVICE'),
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['debug'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+}
